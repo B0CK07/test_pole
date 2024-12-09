0bbeda60f7fec6d4b1911e2d1b0cc95840168843
Daniel Bock <Daniel.Bock@student.souepl.cz>
2024-12-09 09:32:07 +0100

    ggs

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const nabidka = ["CPU", "GPU", "RAM", "PSU", "SSD"];
+const kosik = [];
+ 
+function vypis(){
+
+}
+
+function add(){
+
+}
+
+function clear(){
+
+}
+
+vypis(kosik, "kosik-kontent");
+vypis(nabidka, "content")
